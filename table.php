e5cd7ffb31112e5e6aced78f00c4c75dff9c38d3
richasojitra <154963674+richasojitra@users.noreply.github.com>
2024-01-02 10:45:51 +0530

    Add files via upload

---
+++ table.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+<form action="tablle.php" method="get">
+Enter Firstname:<input type="text" name="first"/><br><br>
+Enter Lastname:<input type="text" name="last"/><br><br>
+Enter Address:<input type="text" name="address"/><br><br>
+Enter city:<input type="text" name="city"/><br><br>
+Enter state:<input type="text" name="state"/><br><br>
+Enter Mobile:<input type="text" name="mobile"/><br><br>
+Enter class:<input type="text" name="class"/><br><br>
+Enter Division:<input type="text" name="divi"/><br><br>
+<input type="submit" name="submit"/>
+</form>
+</body>
+
+</html>
+
+
+<?php
+ 
+ 
+?>
---
+++ tablle.php
@@ -0,0 +1,13 @@
+<?php
+echo  $first_name=$_REQUEST['first'];
+echo $last_name=$_REQUEST['last'];
+echo  $address=$_REQUEST['address'];
+echo  $city=$_REQUEST['city'];
+echo  $state=$_REQUEST['state'];
+echo  $mobile=$_REQUEST['mobile'];
+echo  $class=$_REQUEST['city'];
+echo  $division=$_REQUEST['divi'];
+  
+echo "<table>
+<tr><td>first_name:</td>"
+?>
