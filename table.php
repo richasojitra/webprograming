baa571402a38a3fab382eccebf110986996d9c76
richasojitra <154963674+richasojitra@users.noreply.github.com>
2024-01-10 11:10:44 +0530

    Add files via upload

--- table.php
+++ table.php
@@ -1,22 +1,12 @@
 <html>
+<head>
 <body>
-<form action="tablle.php" method="get">
-Enter Firstname:<input type="text" name="first"/><br><br>
-Enter Lastname:<input type="text" name="last"/><br><br>
-Enter Address:<input type="text" name="address"/><br><br>
-Enter city:<input type="text" name="city"/><br><br>
-Enter state:<input type="text" name="state"/><br><br>
-Enter Mobile:<input type="text" name="mobile"/><br><br>
-Enter class:<input type="text" name="class"/><br><br>
-Enter Division:<input type="text" name="divi"/><br><br>
-<input type="submit" name="submit"/>
+
+<form method="get" action="tablee.php">
+start number:<input type="text" name="sno"><br>
+End number:<input type="text" name="lno"><br>
+<input type="submit" name="submit">
 </form>
 </body>
-
+</head>
 </html>
-
-
-<?php
- 
- 
-?>
