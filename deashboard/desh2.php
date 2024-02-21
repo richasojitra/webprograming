6f83490abf35b29843ba45044b3f4166995b0d42
richasojitra <154963674+richasojitra@users.noreply.github.com>
2024-02-21 11:18:56 +0530

    Add files via upload

---
+++ deashboard/desh2.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<body>
+<form  action="desh.php" method="post">
+about me:<input type="text" name="aboutme"/><br>
+Skill set:<input type="text" name="myskill"/><br>
+<input type="submit" name="submit"/><br>
+</form>
+</body>
+</html>
+
+
+
+
